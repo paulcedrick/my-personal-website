f418d25ad3c6105b5212dfe3e83831cc34562ac7
Paul Cedrick Artigo <paulcedrick.artigo@gmail.com>
2022-12-06 22:23:22 +0800

    added additional meta tags

--- website/app/head.tsx
+++ website/app/head.tsx
@@ -3,7 +3,24 @@ export default function Head() {
     <>
       <title>PCA | Portfolio</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="title" content="PCA's Portfolio" />
       <meta name="description" content="I build things for the web" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://paulcedrick.vercel.app" />
+      <meta property="og:title" content="PCA's Portfolio" />
+      <meta property="og:description" content="I build things for the web" />
+      <meta property="og:image" content="/logo-small.png" />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://paulcedrick.vercel.app" />
+      <meta property="twitter:title" content="PCA's Portfolio" />
+      <meta
+        property="twitter:description"
+        content="I build things for the web"
+      />
+      <meta property="twitter:image" content="/logo-small.png" />
+
       <link rel="icon" href="/favicon.ico" />
     </>
   );
